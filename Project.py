f58c7fb33c063140d2d35267b9323f76796d1f82
RudraFF07 <72027192+RudraFF07@users.noreply.github.com>
2022-09-01 19:05:20 +0530

    Create Project.py

---
+++ Project.py
@@ -0,0 +1,111 @@
+
+'''
+Question -1 : Write a program to print your name using varibale
+'''
+# Write down from here
+myName = "Reyansh"
+print(myName)
+
+
+
+
+
+
+
+''''
+Question - 2 : Write a program to print your name
+'''
+#Write Down From Here
+print("Reyansh")
+
+
+
+
+
+
+'''
+Question - 3 : Write a program to add two numbers
+'''
+#Write down from here 
+print("Enter Numbers ")
+firstNum = int(input("Enter First Number : "))
+secondNum = int(input("Enter Second Number : "))
+
+print("The result is :",firstNum + secondNum)
+
+
+
+
+'''
+Question - 4 : Write a program to findout the circumference of circle 
+'''
+#Write Down From Here
+
+radius = float(input('Enter the input : '))
+
+circumference = 2 * 3.14 * radius 
+
+print("Circumference Of Circle Is : ", circumference)
+
+
+
+
+'''
+Question - 5 : Write your name with input function
+'''
+#Write Down From Here 
+
+
+myName = input("Type Your Name")
+
+print(myName)
+
+
+'''
+Question - 6 : Write a program for finding the result by taking marks of two subject
+'''
+#Write Down From Here
+print("Enter Your Marks Out of 100")
+firstSub = int(input('Enter First Subject Marks : '))
+secondSub = int(input('Enter Second Subejet Marks : '))
+
+totalMarks = firstSub + secondSub 
+
+marksPercent = totalMarks * 100 / 200
+
+print("Total obtained marks : ",totalMarks ,"\n Result Percentage",marksPercent)
+
+
+
+'''
+Question - 7 : Create a list of students 
+'''
+# Write down from here 
+
+students = ["Reyansh","Mitansh","Advik","Aaradhya","Pragya","Anya"]
+
+print(students)
+
+
+
+
+'''
+Question - 8 : Adding an item in a lost using append function
+'''
+# Write from down here
+
+students = ["Reyansh","Mitansh","Advik","Aaradhya","Pragya","Ananya"] 
+students.append("Ivaan")
+
+print(students)
+
+
+
+
+'''
+Question - 9 : Find out the first element from a list 
+'''
+
+nameOfCountry = ["India","Germany","France","Indonesia","Belgium","Austria"]
+
+print("First Element Of List Is : ", nameOfCountry[0])
